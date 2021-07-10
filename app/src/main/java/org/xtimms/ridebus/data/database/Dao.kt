09bfc0db4029214164abe21f96c6ae1396736475
ztimms73 <vp1984tanki@gmail.com>
2021-07-10 22:15:57 +0300

    Added necessary queries

--- app/src/main/java/org/xtimms/ridebus/data/database/Dao.kt
+++ app/src/main/java/org/xtimms/ridebus/data/database/Dao.kt
@@ -2,6 +2,7 @@ package org.xtimms.ridebus.data.database
 
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface Dao {
@@ -23,15 +24,21 @@ interface Dao {
     @Query("SELECT * FROM route WHERE (city_id=1 AND transport_id=4) ORDER BY LENGTH(routeNumber), routeNumber")
     fun getRouteTaxis(): List<Route>
 
-    @Query("SELECT route.* FROM routeStops INNER JOIN route on route._id = routeStops.route_id WHERE stop_id = :stopId")
+    @Query("SELECT route.* FROM routeStops" +
+            " INNER JOIN route on route._id = routeStops.route_id WHERE stop_id = :stopId")
     fun getRoutesByStop(stopId: Int): List<Route>
 
-    @Query("SELECT stop.* FROM routeStops INNER JOIN stop ON stop._id = routeStops.stop_id WHERE routeStops.route_id = :routeId ORDER BY stopNumber ASC")
+    @Query("SELECT stop.* FROM routeStops INNER JOIN stop ON stop._id = routeStops.stop_id " +
+            "WHERE routeStops.route_id = :routeId ORDER BY stopNumber ASC")
     fun getStops(routeId: Int): List<Stop>
 
-    @Query("SELECT schedule.time FROM routeStops INNER JOIN schedule ON (schedule.routeStop_id = routeStops._id and schedule.typeDay_id = :typeDay) WHERE (routeStops.route_id = :routeId and routeStops.stop_id = :stop_id)")
+    @Query("SELECT schedule.time FROM routeStops INNER JOIN schedule " +
+            "ON (schedule.routeStop_id = routeStops._id and schedule.typeDay_id = :typeDay) " +
+            "WHERE (routeStops.route_id = :routeId and routeStops.stop_id = :stop_id)")
     fun getTimeOnStop(typeDay: Int, routeId: Int, stop_id: Int): List<String>
 
-    /*@Query("SELECT schedule.typeDay_id FROM schedule WHERE schedule.routeStop_id = (SELECT routeStops._id FROM routeStops WHERE route_id = :routeId AND stop_id = :stop_id GROUP BY schedule.typeDay_id")
-    fun getTypeDay(routeId: Int, stop_id: Int): Flow<List<Int>>*/
+    @Query("SELECT schedule.typeDay_id FROM schedule WHERE schedule.routeStop_id = " +
+            "(SELECT routeStops._id FROM routeStops WHERE route_id = :routeId AND stop_id = :stop_id)" +
+            " GROUP BY schedule.typeDay_id")
+    fun getTypeDay(routeId: Int, stop_id: Int): List<Int>
 }
