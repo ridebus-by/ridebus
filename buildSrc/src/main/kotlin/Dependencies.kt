66e5e2a1ac79e252c98e9dc614de815b30c37694
Zakhar Timoshenko <vp1984tanki@gmail.com>
2021-08-31 01:41:43 +0300

    Update build plugins

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object BuildPluginsVersion {
-    const val AGP = "7.0.0"
-    const val KOTLIN = "1.5.21"
-    const val KOTLINTER = "3.4.5"
+    const val AGP = "7.0.1"
+    const val KOTLIN = "1.5.30"
+    const val KOTLINTER = "3.5.1"
     const val ABOUTLIB_PLUGIN = "8.9.1"
 }
