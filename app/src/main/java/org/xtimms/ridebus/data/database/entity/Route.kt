1a0888462f2908cd238a846c1277165355e49f4a
Zakhar Timoshenko <vp1984tanki@gmail.com>
2021-09-19 07:07:37 +0300

    The route information screen is almost done

--- app/src/main/java/org/xtimms/ridebus/data/database/entity/Route.kt
+++ app/src/main/java/org/xtimms/ridebus/data/database/entity/Route.kt
@@ -16,5 +16,12 @@ data class Route(
     @ColumnInfo(name = "kind_id") val kindId: Int,
     @ColumnInfo(name = "number") val number: String,
     @ColumnInfo(name = "title") val title: String,
-    @ColumnInfo(name = "description") val description: String
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "payment_methods") val paymentMethods: String,
+    @ColumnInfo(name = "fare") val fare: String,
+    @ColumnInfo(name = "weekly_traffic") val weeklyTraffic: String,
+    @ColumnInfo(name = "working_hours") val workingHours: String,
+    @ColumnInfo(name = "following") val following: String,
+    @ColumnInfo(name = "carrier_company") val carrierCompany: String,
+    @ColumnInfo(name = "tech_info") val techInfo: String
 )
