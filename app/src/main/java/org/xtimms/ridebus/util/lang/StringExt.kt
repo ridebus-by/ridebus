ced2120a02ee0ab023a12c1a707f556e70aea644
Zakhar Timoshenko <vp1984tanki@gmail.com>
2021-12-13 16:56:17 +0300

    Add transliterate func (unused temporarily)

--- app/src/main/java/org/xtimms/ridebus/util/lang/StringExt.kt
+++ app/src/main/java/org/xtimms/ridebus/util/lang/StringExt.kt
@@ -1,6 +1,7 @@
 package org.xtimms.ridebus.util.lang
 
-import java.lang.Math.floor
+import java.util.*
+import kotlin.math.floor
 
 fun String.truncateCenter(count: Int, replacement: String = "..."): String {
     if (length <= count) {
@@ -10,4 +11,29 @@ fun String.truncateCenter(count: Int, replacement: String = "..."): String {
     val pieceLength: Int = floor((count - replacement.length).div(2.0)).toInt()
 
     return "${take(pieceLength)}$replacement${takeLast(pieceLength)}"
+}
+
+fun String.transliterate(skipMissing: Boolean): String {
+    val cyr = charArrayOf(
+        'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
+        'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'ё'
+    )
+    val lat = arrayOf(
+        "a", "b", "v", "g", "d", "e", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p",
+        "r", "s", "t", "u", "f", "h", "ts", "ch", "sh", "sch", "", "i", "", "e", "ju", "ja", "jo"
+    )
+    return buildString(length + 5) {
+        for (c in this@transliterate) {
+            val p = cyr.binarySearch(c.toLowerCase())
+            if (p in lat.indices) {
+                if (c.isUpperCase()) {
+                    append(lat[p].toUpperCase(Locale.getDefault()))
+                } else {
+                    append(lat[p])
+                }
+            } else if (!skipMissing) {
+                append(c)
+            }
+        }
+    }
 }
