162d37ca4b19c3b4eb2f7d680216a3141c14479c
Zakhar Timoshenko <vp1984tanki@gmail.com>
2022-01-02 22:25:05 +0300

    Update about library plugin

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,5 +2,5 @@ object BuildPluginsVersion {
     const val AGP = "7.0.4"
     const val KOTLIN = "1.6.10"
     const val KOTLINTER = "3.6.0"
-    const val ABOUTLIB_PLUGIN = "8.9.3"
+    const val ABOUTLIB_PLUGIN = "8.9.4"
 }
